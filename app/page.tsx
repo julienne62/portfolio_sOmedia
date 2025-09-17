c68c854fd6b262dbe16ba802b51e57c9df305704
Crimra <smithmounkassa2@gmail.com>
2025-09-17 01:03:25 +0100

    Initialisation correct

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <p>Hello, Rabbi</p>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -18,8 +19,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
-            .
-          </li>
+                     </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
